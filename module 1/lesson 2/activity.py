d93fe81db55c9eedb149d68f4c726e2cd9068414
Blackpanther1311 <paarthrajpoot13@gmail.com>
2025-10-29 21:43:57 +0530

    lesson 2

---
+++ module 1/lesson 2/activity.py
@@ -0,0 +1,14 @@
+# creating variable
+x = 5
+myname ="john" 
+print(x)
+print(myname)
+
+#user input
+name =input("enter your name :")
+
+print("your name is ",name)
+
+import keyword
+#prnting keyword list
+print(keyword.kwlist)

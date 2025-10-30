326df60a230a63d58e1540a4b7426942b544907c
Blackpanther1311 <paarthrajpoot13@gmail.com>
2025-10-30 21:32:40 +0530

    lesson 3

---
+++ module 1/lesson 3/activity.py
@@ -0,0 +1,18 @@
+x=5
+
+print("the data type of x is",type(x))
+
+y =1.55
+print("the data type of y is",type(y))
+
+z="codeingal"
+print("the data type of z is ",type(z))
+
+done=True
+print("the data type of done is",type(done))
+
+y=int(y)
+print("the data type of y is ",type(y))
+
+done=str(done)
+print("the date type of done is",type(done))

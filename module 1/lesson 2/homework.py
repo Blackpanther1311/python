2c50e0f094fc273aeb3f1f77cb3851da72bec46b
Blackpanther1311 <paarthrajpoot13@gmail.com>
2025-11-12 18:15:21 +0530

    i love cat

--- module 1/lesson 2/homework.py
+++ module 1/lesson 2/homework.py
@@ -1,2 +1,11 @@
-tejas="11.november" 
+tejas="9.november" 
 print("tejas birthaday is ",tejas)
+
+paarth="11.november"
+print("paarth birthaday is",paarth)
+
+hardik="12.june"
+print("hardik birthaday is",hardik)
+
+arnove="13.januvary" 
+print("arnove birthaday is",arnove)

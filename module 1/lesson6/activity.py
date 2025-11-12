cdac1772733fff4ac89e6ff53594d1e4308fe452
Blackpanther1311 <paarthrajpoot13@gmail.com>
2025-11-12 21:21:15 +0530

    i love cat and dog

---
+++ module 1/lesson6/activity.py
@@ -0,0 +1,11 @@
+a = 5
+b = 6
+
+if a==5 and b==6:
+    print("both conditioions are true")
+if a==5 or b==10:
+    print("at lest one condition is true")
+if a !=10:
+    print("a is not equal yo 10")
+
+

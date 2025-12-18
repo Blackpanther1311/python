a545191b67f23b3056d9c75880c47d0f9afc0567
Blackpanther1311 <paarthrajpoot13@gmail.com>
2025-12-18 21:59:33 +0530

    mmmmhhhhh

---
+++ module 2/homework.py
@@ -0,0 +1,15 @@
+import turtle
+
+turtle.Screen().bgcolor("pink")
+turtle.Screen().setup(500,500)
+square=turtle.Turtle()
+
+square.forward(100)
+square.right(90)
+square.forward(100)
+square.right(90)
+square.forward(100)
+square.right(90)
+square.forward(100)
+
+turtle.done()

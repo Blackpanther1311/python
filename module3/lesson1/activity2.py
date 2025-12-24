ad306858e9f9ac02afa23ca83fc74d14825c542f
Blackpanther1311 <paarthrajpoot13@gmail.com>
2025-12-24 21:25:43 +0530

    hmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm

---
+++ module3/lesson1/activity2.py
@@ -0,0 +1,21 @@
+def weather_condition():
+    print("the weather is hot",sunny)
+    print("the weather is very cold in",winter)
+
+
+
+
+
+
+
+
+
+
+
+
+
+sunny ="sunny"
+winter="winter"
+
+
+weather_condition()

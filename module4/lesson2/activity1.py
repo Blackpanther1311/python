b5c282a90cf4f7fac20c639559bf645292128eb2
Blackpanther1311 <paarthrajpoot13@gmail.com>
2026-01-28 21:33:55 +0530

    h

---
+++ module4/lesson2/activity1.py
@@ -0,0 +1,31 @@
+number_tuple=(10,20,25.5)
+print(number_tuple)
+
+
+
+
+
+
+string_tuple=("paarth","tasmia","jessi")
+print(string_tuple)
+
+
+
+
+#size of the tuple
+print(len(string_tuple))
+
+
+
+
+
+#Accessing a particular element
+print(string_tuple[1])
+
+
+
+
+
+
+#concatenation
+print(number_tuple+string_tuple)

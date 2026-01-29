244f6f18a398396debd5fe95c7b71634cf95a0ec
Blackpanther1311 <paarthrajpoot13@gmail.com>
2026-01-29 20:52:11 +0530

    j

---
+++ module4/lesson3/activity1.py
@@ -0,0 +1,21 @@
+my_dict={'name':'guuci','age':7, 'Grade':'first'}
+
+
+#accesing a particular element
+print(my_dict['name'])
+
+
+#update the value
+my_dict['age']=8
+
+
+#adding an item
+my_dict['gender']='female'
+
+print(my_dict)
+
+
+#delete an iteam
+print(my_dict.pop('Grade'))
+
+print(my_dict)

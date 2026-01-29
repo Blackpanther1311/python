af3795ad9fc9ddb375cdcff9894a277df256c32b
Blackpanther1311 <paarthrajpoot13@gmail.com>
2026-01-29 21:03:25 +0530

    c

---
+++ module4/lesson3/activity2.py
@@ -0,0 +1,10 @@
+country_code ={
+    'india':'0091',
+    'autralia':'0025',
+    'nepal':'00977'
+}
+
+
+print(country_code.get('india','not found'))
+
+print(country_code.get('japan' ,'not found'))

2031895b4ef3a4f19802646c0bfcfdaf56aed45d
Blackpanther1311 <paarthrajpoot13@gmail.com>
2026-02-04 21:38:01 +0530

    hhhhhhhhhhhhhhh

---
+++ module4/lesson4/activity2.py
@@ -0,0 +1,16 @@
+set1={'red','green','blue'}
+set2= {'green','purpul','brown'}
+
+
+
+
+print(set1.union(set2))
+
+
+print(set1.intersection(set2))
+
+
+print(set.difference(set2))
+
+
+print(set1.symmetric_difference(set2))

4195e82e8b7e4520ce97632f376bd4a4f0c2d0a2
Blackpanther1311 <paarthrajpoot13@gmail.com>
2026-02-04 21:19:03 +0530

    nhgyrtyrwewswqazxswedcvfrtgbhhyujm,kiiol.p['//211474788565

---
+++ module4/lesson4/activity.py
@@ -0,0 +1,18 @@
+my_set={11,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,3,3,3,3,3,4,4,4,4,4,4,4,4,4,5,5,5,5,5,5,5,5,5,5}
+
+
+print(my_set)
+
+
+my_set1={1.0,'tasmia mam',45}
+print(my_set1)
+
+#Deleting the first element
+my_set.pop()
+print(my_set)
+
+
+
+my_list=[1,2,3,4,5,6,7,8,9,0]
+new_set=set(my_list)
+print(new_set)

e43623593f054d92ab65f285a184060c70f7ec05
Blackpanther1311 <paarthrajpoot13@gmail.com>
2026-03-18 21:29:04 +0530

    1010100101010100101

---
+++ module5/lesson4/activity1.py
@@ -0,0 +1,25 @@
+from abc import ABC,  abstractmethod
+
+class Absclass(ABC):
+
+
+    def print(self ,x):
+        print("passed value:",x)
+
+    @abstractmethod
+    def task(self):
+        print("we are inside absclass")
+
+
+class test_class(Absclass):
+    def task(self):
+        print("we are inside the test_class")
+
+
+
+
+
+
+test_obj = test_class()
+test_obj.task()
+test_obj.print(101010101010100)
